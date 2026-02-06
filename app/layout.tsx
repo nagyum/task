7aef7677943b24084a0c0357b749181cf93e4ba3
nagyum <wildanimal19@naver.com>
2026-02-06 13:24:51 +0900

    feat:메타데이터 추가

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,10 @@
 import Header from "@/src/components/page/home/Header";
 import type { ReactNode } from "react";
+import type { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "빅스페이먼츠 과제",
+};
 
 export default function RootLayout({ children }: { children: ReactNode }) {
   return (
